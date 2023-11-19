191361138ba01dd05b9908ec3cffb41116b0a2db
Jason Ban Tze Tan <jtbt1218@gmail.com>
2023-11-18 22:44:53 -1000

    Server send file size and content, and client prints file content

--- client.c
+++ client.c
@@ -10,69 +10,89 @@
 #define PORT 8080
 #define SA struct sockaddr
 
-void func(int sockfd) {
-    char buff[MAX];
-    int n;
+void receiveFile(int sockfd) {
+    char buffer[MAX];
+    int file_size = 0;
+    int bytes_received = 0;
 
-    for (;;) {
-        bzero(buff, sizeof(buff));
-        printf("Enter the filename (or 'exit' to quit): ");
-        n = 0;
-        while ((buff[n++] = getchar()) != '\n') {
-            if (n >= MAX - 2) { // To prevent buffer overflow
-                printf("Filename is too long.\n");
-                break;
-            }
-        }
+    // Read the file size from the server
+    read(sockfd, buffer, sizeof(buffer));
+    file_size = atoi(buffer); // Convert string to integer for file size
 
-        // Check for 'exit' command
-        if (strncmp(buff, "exit", 4) == 0) {
-            printf("Client Exit...\n");
+    printf("File size received from server: %d bytes\n", file_size);
+
+    // Read the file content from the server
+    printf("File content received from server:\n");
+
+    while (bytes_received < file_size) {
+        bzero(buffer, MAX);
+        int bytes_to_read = (file_size - bytes_received) > MAX ? MAX : (file_size - bytes_received);
+        int bytes = read(sockfd, buffer, bytes_to_read);
+        if (bytes <= 0) {
+            printf("Error in reading the file from server.\n");
             break;
         }
+        bytes_received += bytes;
 
-        // Append flag at the end of filename
-        strcat(buff, "$");
-
-        write(sockfd, buff, sizeof(buff));
-        bzero(buff, sizeof(buff));
-        read(sockfd, buff, sizeof(buff));
-        printf("From Server: %s\n", buff);
+        // Print the received content (assuming it's text-based)
+        printf("%.*s", bytes, buffer);
     }
+
+    printf("\nFile received from server.\n");
+}
+
+void requestFile(int sockfd, const char *filename) {
+    // Send the filename to the server
+    write(sockfd, filename, strlen(filename) + 1);
+
+    // Receive file from the server
+    receiveFile(sockfd);
 }
 
 int main() {
     int sockfd;
     struct sockaddr_in servaddr;
+    char filename[MAX];
 
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
+    while (1) {
+        // socket create and verification
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1) {
+            printf("socket creation failed...\n");
+            exit(0);
+        } else
+            printf("Socket successfully created..\n");
+        bzero(&servaddr, sizeof(servaddr));
+
+        // assign IP, PORT
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        servaddr.sin_port = htons(PORT);
+
+        // connect the client socket to server socket
+        if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0) {
+            printf("connection with the server failed...\n");
+            exit(0);
+        } else
+            printf("connected to the server..\n");
+
+        // Request a file from the server
+        printf("Enter the filename to request from the server (or type 'exit' to quit): ");
+        fgets(filename, MAX, stdin);
+        filename[strcspn(filename, "\n")] = '\0'; // Remove newline character if present
+
+        // Check if the user wants to exit
+        if (strcmp(filename, "exit") == 0) {
+            close(sockfd);
+            break;
+        }
+
+        // Send the filename to the server and receive the file
+        requestFile(sockfd, filename);
+
+        // close the socket
+        close(sockfd);
     }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
-
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    servaddr.sin_port = htons(PORT);
-
-    // connect the client socket to server socket
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
-        printf("connection with the server failed...\n");
-        exit(0);
-    }
-    else
-        printf("connected to the server..\n");
-
-    // function for chat
-    func(sockfd);
-
-    // close the socket
-    close(sockfd);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,96 +1,117 @@
-#include <stdio.h> 
-#include <netdb.h> 
-#include <netinet/in.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h> // read(), write(), close()
-#define MAX 80 
-#define PORT 8080 
-#define SA struct sockaddr 
-   
-// Function designed for chat between client and server. 
-void func(int connfd) {
-    char buff[MAX];
+#include <pthread.h> // For POSIX threads
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+
+// Function designed for handling each client request
+void *handle_request(void *arg) {
+    int connfd = *((int *)arg);
+    char filename[MAX];
+    char buffer[MAX];
     int n;
 
-    for (;;) {
-        bzero(buff, MAX);
+    // Read the file name sent by the client
+    read(connfd, filename, sizeof(filename));
+    filename[strcspn(filename, "\n")] = '\0'; // Remove newline character if present
 
-        // Read the file name sent by the client
-        read(connfd, buff, sizeof(buff));
-        
-        // Remove newline character if present at the end
-        buff[strcspn(buff, "\n")] = '\0';
+    printf("File requested by client: %s\n", filename);
 
-        // Check if the received message ends with the flag symbol '$'
-        if (buff[strlen(buff) - 1] == '$') {
-            // Removing the flag symbol '$' from the file name
-            buff[strlen(buff) - 1] = '\0';
-        }
+    FILE *fileptr;
+    fileptr = fopen(filename, "r");
+    if (fileptr == NULL) {
+        printf("File not found or unable to open the file.\n");
+        write(connfd, "File not found or unable to open the file.", sizeof("File not found or unable to open the file."));
+        close(connfd);
+        pthread_exit(NULL);
+    }
 
-        // If the received message contains "exit", server exits and chat ends
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
+    // Calculate the file size
+    fseek(fileptr, 0L, SEEK_END);
+    int file_size = ftell(fileptr);
+    rewind(fileptr);
+
+    // Send the file size to the client followed by a flag
+    char size_msg[MAX];
+    snprintf(size_msg, sizeof(size_msg), "%d$", file_size);
+    write(connfd, size_msg, sizeof(size_msg));
+
+    // Send the file contents to the client
+    while ((n = fread(buffer, sizeof(char), sizeof(buffer), fileptr)) > 0) {
+        if (write(connfd, buffer, n) < 0) {
+            printf("Error sending file.\n");
             break;
         }
-
-        printf("Received file name from client: %s\n", buff);
     }
+
+    fclose(fileptr); // Close the file
+    close(connfd);   // Close the connection
+
+    printf("File sent to client.\n");
+    pthread_exit(NULL);
 }
 
-   
-// Driver function 
-int main() 
-{ 
-    int sockfd, connfd, len; 
-    struct sockaddr_in servaddr, cli; 
-   
-    // socket create and verification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
-   
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
-   
-    // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully binded..\n"); 
-   
-    // Now server is ready to listen and verification 
-    if ((listen(sockfd, 5)) != 0) { 
-        printf("Listen failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Server listening..\n"); 
-    len = sizeof(cli); 
-   
-    // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (SA*)&cli, &len); 
-    if (connfd < 0) { 
-        printf("server accept failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("server accept the client...\n"); 
-   
-    // Function for chatting between client and server 
-    func(connfd); 
-   
-    // After chatting close the socket 
-    close(sockfd); 
+int main() {
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+    socklen_t len;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    } else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
+    } else
+        printf("Socket successfully binded..\n");
+
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
+    } else
+        printf("Server listening..\n");
+
+    len = sizeof(cli);
+
+    while (1) {
+        // Accept the data packet from client and verification
+        connfd = accept(sockfd, (SA *)&cli, &len);
+        if (connfd < 0) {
+            printf("server accept failed...\n");
+            exit(0);
+        } else
+            printf("server accept the client...\n");
+
+        // Create a thread to handle the client request
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, handle_request, &connfd) != 0) {
+            printf("Failed to create thread for handling request.\n");
+            close(connfd);
+        }
+    }
+
+    // close the socket
+    close(sockfd);
+
+    return 0;
 }
